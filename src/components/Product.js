ff0493543938b2f799fdc06e96311f849af4b7a6
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-29 20:00:59 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -23,10 +23,16 @@ Product.propTypes= {
   hasWatermark: PropTypes.bool,
   color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
   weight: (props, propName) => {
-     const weight = props[propName];
-     const isValidWeight = weight > 80 && weight < 300;
-       if (!isValidWeight) {
-        return new Error('The `weight` prop should range between 80 and 300.');
-      }
-    }
+     const weight = props[propName]
+     if (weight === undefined) {
+       return new Error('The `weight` prop is required.')
+     }
+     if (isNaN(weight)) {
+       return new Error('The `weight` prop is not a number.')
+     }
+     const isValidWeight = weight > 80 && weight < 300
+     if (!isValidWeight) {
+       return new Error('The `weight` prop should range between 80 and 300.')
+     }
+   }
 }
