dbe40b0ef7a8d5c530b257dff0f971ae47b957e4
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-29 19:52:37 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,16 @@
-// Code Product Component Here
+import PropTypes from 'prop-types'
+import React from 'react'
+
+export default class Product extends React.Component {
+  render() {
+    return(
+      <div>
+        <ul>
+          <li>{this.props.name}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+
